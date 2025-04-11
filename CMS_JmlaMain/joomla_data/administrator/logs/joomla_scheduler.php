9281e38efce77ec9d50a4c9edb26891ca68bd852
Sabin <sabinstan19@gmail.com>
2025-04-11 20:00:58 +0300

    test all in docekr

--- CMS_JmlaMain/joomla_data/administrator/logs/joomla_scheduler.php
+++ CMS_JmlaMain/joomla_data/administrator/logs/joomla_scheduler.php
@@ -9,3 +9,10 @@
 2025-04-09	16:16:17	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
 2025-04-09	16:16:31	INFO	Running task#03 'Update Notification'.
 2025-04-09	16:16:39	INFO	Successfully finished task#03 in 8.04 (net 8.04) seconds.
+2025-04-11	15:59:56	INFO	Running task#02 'Session GC'.
+2025-04-11	15:59:56	INFO	Task> SessionGC end
+2025-04-11	15:59:56	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2025-04-11	16:00:00	INFO	Running task#03 'Update Notification'.
+2025-04-11	16:00:21	INFO	Task> Could not instantiate mail function.
+2025-04-11	16:00:21	INFO	Task> UpdateNotification end
+2025-04-11	16:00:21	INFO	Successfully finished task#03 in 21.55 (net 21.55) seconds.
